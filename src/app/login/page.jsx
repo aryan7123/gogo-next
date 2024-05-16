7aca81802601d509eeb75c38edbbdcdbe0ba3fcd
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-05-16 18:58:27 +0530

    login and register page completed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,20 +1,76 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
     <>
-        <main className='w-full h-full flex items-center fixed-background'>
-            <div className='max-w-sm md:max-w-6xl flex md:flex-row flex-col items-start justify-center mx-auto bg-white form-card'>
-                <div className='w-full md:w-[40%] md:px-12 md:py-16 p-6 h-[30vh] md:h-[70vh] image-side'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis molestiae, animi doloremque asperiores sapiente expedita iusto doloribus! Aliquam, nam facere!
-                </div>
-                <div className='w-full md:w-[60%] form-side'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, atque rem debitis sequi ad maxime pariatur sint in, officiis illo, commodi earum hic fugit omnis? Tempora, quasi sapiente ipsam, harum, adipisci deleniti quam corrupti fuga ea tenetur dolorum. Aperiam tempore consequuntur dolor deleniti nisi explicabo doloremque quidem eum magnam, iusto dolorum placeat suscipit consectetur neque tempora laboriosam. Voluptatum illum in tenetur voluptate ratione, voluptas fugiat esse earum doloremque. Quo necessitatibus excepturi hic dicta in doloremque nobis? Rem, quibusdam. Qui magnam est ex facere? Tempore ad doloribus suscipit magni, repellat dignissimos quas eaque reiciendis nobis ea incidunt cupiditate perferendis in enim!
-                </div>
-            </div>
-        </main>
+      <main className="w-full h-full flex items-center justify-center fixed-background">
+        <div className="max-w-sm md:max-w-[1400px] flex md:flex-row flex-col items-start justify-center mx-auto bg-white form-card">
+          <div className="w-full md:w-[40%] md:pr-[18rem] md:pl-12 md:py-16 py-8 pr-8 pl-8 md:h-[70vh] image-side">
+            <h3 className="text-white text-2xl mb-2 md:text-3xl font-medium whitespace-nowrap">
+              MAGIC IS IN THE <br className="hidden md:block" /> DETAILS
+            </h3>
+            <span className="text-white text-sm whitespace-nowrap">
+              Please use your credentials to login.
+            </span>
+            <br />
+            <span className="text-white text-sm whitespace-nowrap">
+              If you are not a member,{" "}
+              <Link href="/register" className="underline">
+                please register
+              </Link>
+            </span>
+          </div>
+          <div className="w-full md:w-[60%] md:pl-16 md:pr-[5rem] md:py-16 py-8 pr-8 pl-8">
+            <span className="gogo-logo mb-5"></span>
+            <span className="text-[#3a3a3a] text-sm font-medium">LOGIN</span>
+            <form className="mt-8">
+              <div className="flex flex-col gap-1.5 mb-6">
+                <label
+                  htmlFor="email"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <label
+                  htmlFor="password"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="email"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="flex items-center justify-between w-[inherit] mt-6">
+                <Link
+                  href="/forgot-password"
+                  className="text-[#3a3a3ab3] text-sm font-medium hover:text-[#922c88] transition-colors"
+                >
+                  Forgot Password?
+                </Link>
+                <button
+                  type="button"
+                  className="bg-[#922c88] text-white w-[120px] text-sm font-medium rounded-[50px] py-2.5 shadow-sm transition-colors hover:bg-[#73236b]"
+                >
+                  LOGIN
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <main className="w-full h-full flex items-center justify-center fixed-background">
+        <div className="max-w-sm md:max-w-[1400px] flex md:flex-row flex-col items-start justify-center mx-auto bg-white form-card">
+          <div className="w-full md:w-[40%] md:pr-[20rem] md:pl-12 md:py-16 py-8 pr-8 pl-8 md:h-[95vh] image-side">
+            <h3 className="text-white text-2xl mb-2 md:text-3xl font-medium whitespace-nowrap">
+              MAGIC IS IN THE <br className="hidden md:block" /> DETAILS
+            </h3>
+            <span className="text-white text-sm whitespace-nowrap">
+              Please use this form to register.
+            </span>
+            <br />
+            <span className="text-white text-sm whitespace-nowrap">
+              If you are a member,{" "}
+              <Link href="/login" className="underline">
+                please login
+              </Link>
+            </span>
+          </div>
+          <div className="w-full md:w-[60%] md:pl-16 md:pr-[6rem] md:py-16 py-8 pr-8 pl-8">
+            <span className="gogo-logo mb-5"></span>
+            <span className="text-[#3a3a3a] text-sm font-medium">REGISTER</span>
+            <form className="mt-8">
+              <div className="flex flex-col gap-1.5 mb-6">
+                <label
+                  htmlFor="fullName"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  name="fullName"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="flex flex-col gap-1.5 mb-6">
+                <label
+                  htmlFor="email"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="flex flex-col gap-1.5 mb-6">
+                <label
+                  htmlFor="password"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="email"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <label
+                  htmlFor="confirmPassword"
+                  className="text-[#3a3a3ab3] text-xs font-medium"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
+                />
+              </div>
+              <div className="w-[inherit] flex justify-end mt-6">
+                <button
+                  type="button"
+                  className="bg-[#922c88] text-white w-[120px] text-sm font-medium rounded-[50px] py-2.5 shadow-sm transition-colors hover:bg-[#73236b]"
+                >
+                  REGISTER
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default page
+export default page;
