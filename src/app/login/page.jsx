b0c0457ce668663d0009e5101c97689a09f42b37
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-05-16 14:46:25 +0530

    pushing the project to github

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <main className='w-full h-full fixed-background'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, deserunt quod! Laboriosam voluptatibus sint saepe repudiandae, accusamus excepturi ipsum recusandae amet voluptatem fugiat earum magni incidunt ut quia. Rem, placeat quas. Enim ullam beatae unde, at facere adipisci modi dolorum, totam officiis architecto accusamus eos hic tempore libero consequuntur itaque accusantium saepe expedita dolores illum nihil, similique dolor aperiam delectus? Ea cum quis ducimus id maxime dolorem maiores eligendi. Voluptatibus, rerum soluta! A, corporis illo! Quo doloremque rem minima accusamus error molestias, natus aspernatur atque nisi nam dolore facere fugit quos dolor optio at magnam voluptatem quas ullam consectetur. Voluptas!
+        </main>
+    </>
+  )
+}
+
+export default page
