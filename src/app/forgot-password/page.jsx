df8cd792c2eb5c7e627b5e0be0d28f583c760a2b
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-05-18 21:20:08 +0530

    creating the forgot and create password page

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import Link from "next/link";
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import React from 'react'
+import Link from 'next/link'
 
 const page = () => {
   return (
     <>
-      <main className="w-full h-full flex items-center justify-center fixed-background">
+        <main className="w-full h-full flex items-center justify-center fixed-background">
         <div className="max-w-sm md:max-w-[1400px] flex md:flex-row flex-col items-start justify-center mx-auto bg-white form-card">
           <div className="w-full md:w-[40%] md:pr-[18rem] md:pl-12 md:py-16 py-8 pr-8 pl-8 md:h-[70vh] image-side">
             <h3 className="text-white text-2xl mb-2 md:text-3xl font-medium whitespace-nowrap">
@@ -24,7 +23,7 @@ const page = () => {
           </div>
           <div className="w-full md:w-[60%] md:pl-16 md:pr-[5rem] md:py-16 py-8 pr-8 pl-8">
             <span className="gogo-logo mb-5"></span>
-            <span className="text-[#3a3a3a] text-sm font-medium">LOGIN</span>
+            <span className="text-[#3a3a3a] text-sm font-medium uppercase">forgot password</span>
             <form className="mt-8">
               <div className="flex flex-col gap-1.5 mb-6">
                 <label
@@ -40,32 +39,12 @@ const page = () => {
                   autoComplete="off"
                 />
               </div>
-              <div className="flex flex-col gap-1.5">
-                <label
-                  htmlFor="password"
-                  className="text-[#3a3a3ab3] text-xs font-medium"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="email"
-                  className="border border-[#d7d7d7] p-1.5 text-sm text-[#3a3a3a] outline-none"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="flex items-center justify-between w-[inherit] mt-6">
-                <Link
-                  href="/forgot-password"
-                  className="text-[#3a3a3ab3] text-sm font-medium hover:text-[#922c88] transition-colors"
-                >
-                  Forgot Password?
-                </Link>
+              <div className="flex items-center justify-end w-[inherit] mt-6">
                 <button
                   type="button"
-                  className="bg-[#922c88] text-white w-[120px] text-sm font-medium rounded-[50px] py-2.5 shadow-sm transition-colors hover:bg-[#73236b]"
+                  className="bg-[#922c88] text-white uppercase w-[120px] text-sm font-medium rounded-[50px] py-2.5 shadow-sm transition-colors hover:bg-[#73236b]"
                 >
-                  LOGIN
+                  send
                 </button>
               </div>
             </form>
@@ -73,7 +52,7 @@ const page = () => {
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default page;
+export default page
